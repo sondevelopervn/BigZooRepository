b8eaa92462fde56bc87030c0da199c9574cd9913
hungson12007 <hungson12007@gmail.com>
2015-12-12 13:21:38 +0700

    comment branch1

--- BigZoo/app/src/main/java/hungsonandroid/com/bigzoo/ConThu.java
+++ BigZoo/app/src/main/java/hungsonandroid/com/bigzoo/ConThu.java
@@ -7,6 +7,7 @@ public class ConThu  {
     public String TenConThu;
     public Integer  Hinh;
     public  String comment;
+    public  String commentbranch1;
 
     public ConThu(String tenConThu, Integer hinh) {
         TenConThu = tenConThu;
